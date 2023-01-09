f4faf81ea64c32aa77f80b1ed421f8fd00c2d2e2
Simzer994 <simonevanno9@gmail.com>
2023-01-09 10:57:33 +0100

    update

--- Series_Functions.py
+++ Series_Functions.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
+import numpy as np
 
 
 def get_data(ticker: str):
@@ -48,7 +49,7 @@ def plot_returns(data: pd.DataFrame, ticker: str=None):
     return None
 
 
-def weighted_hs_var(returns: pd.DataFrame, confidence_level: float):
+def weighted_hs_var(returns: pd.DataFrame, confidence_level: int):
     """ Estime la VaR en utilisant la méthode "Weighted HS" """
 
     # Calculer la moyenne des rendements
@@ -63,7 +64,7 @@ def weighted_hs_var(returns: pd.DataFrame, confidence_level: float):
     sorted_weights = weights.sort_values(ascending=False)
 
     # Calculer l'indice du quantile correspondant au niveau de confiance choisi
-    quantile_index = (confidence_level / 100) * returns.shape[0]
+    quantile_index = int((confidence_level / 100) * returns.shape[0])
 
     # Sélectionner la valeur de rendement correspondante dans le tableau de rendements trié
     var = sorted_returns.iloc[quantile_index]
